a805541a2f5014b8d3398d39a1954e4acbba7227
Hannan Mir <hannanmir2@gmail.com>
2020-08-10 18:25:01 -0500

    heroku config

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,29 @@
 const pg = require('pg');
+const url = require('url');
 
-const config = {
-    database: 'weekend-to-do-app', 
-    host: 'localhost', 
-    port: 5432, 
-    max: 10, 
-    idleTimeoutMillis: 30000 
-};
+let config = {};
+
+if (process.env.DATABASE_URL) {
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        database: params.pathname.split('/')[1],
+        ssl: true,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    };
+} else {
+    config = {
+        database: 'weekend-to-do-app', 
+        host: 'localhost', 
+        port: 5432, 
+        max: 10, 
+        idleTimeoutMillis: 30000 
+    };
+}
 
 const pool = new pg.Pool(config);
 
